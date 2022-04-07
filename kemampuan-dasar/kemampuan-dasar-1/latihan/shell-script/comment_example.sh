9397726d766efa354f9ed6a846df03f06f225081
oksipjogja <familybuah@gmail.com>
2022-04-07 21:28:03 +0700

    update-1

---
+++ kemampuan-dasar/kemampuan-dasar-1/latihan/shell-script/comment_example.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#Add two numeric value
+((sum=25+35))
+#Print the result
+echo $sum
---
+++ kemampuan-dasar/kemampuan-dasar-1/latihan/shell-script/splash.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo 'print text with newline'
+echo -n 'print text without newline'
+echo -e '\nRemoving \t backslash \t characters\n'
