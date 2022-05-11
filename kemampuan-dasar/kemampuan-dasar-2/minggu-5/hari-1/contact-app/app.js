d162e1df15968b85d3a35077f2496267af35715e
oksipjogja <familybuah@gmail.com>
2022-05-11 13:02:11 +0700

    part-5

--- kemampuan-dasar/kemampuan-dasar-2/minggu-5/hari-1/contact-app/app.js
+++ kemampuan-dasar/kemampuan-dasar-2/minggu-5/hari-1/contact-app/app.js
@@ -1,3 +1,113 @@
+// mengambil argument dari command line
+const yargs = require('yargs');
+const contacts = require('./contacts');
+
+yargs.command({
+
+    command: 'add',
+    describe: 'Menambahkan contact baru',
+    builder: {
+        nama: {
+            describe: 'Nama lengkap',
+            demandOption: true,
+            type: 'string',
+        },
+        email: {
+            describe: 'Email',
+            demandOption: false,
+            type: 'string',
+        },
+        noHP: {
+            describe: 'No Handphone',
+            demandOption: true,
+            type: 'string',
+        },
+    },
+    handler(argv) {
+        contacts.saveContact(argv.nama, argv.email, argv.noHP);
+    },
+});
+
+yargs.parse();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(yargs.argv);
+// const contact = {
+//     nama: argv.nama,
+//     email: argv.email,
+//     noHP: argv.noHP,
+// };
+// console.log(contact);
+// const yargs = require('yargs/yargs');
+// const { hideBin } = require('yargs/helpers');
+// const argv = yargs(hideBin(process.argv)).argv;
+
+// if (argv.ships > 3 && argv.distance < 53.5) {
+//     console.log('Plunder more riffiwobbles!');
+// } else {
+//     console.log('Retreat for the xupptumblers!')
+// };
+
+// yargs(hideBin(process.argv))
+//     .command('serve [port]', 'start the server', (yargs) => {
+//         return yargs
+//             .positional('port', {
+//                 describe: 'port to bind on',
+//                 default: 5000
+//             })
+//     }, (argv) => {
+//         if (argv.verbose) console.info(`start server on :${argv.port}`)
+//         serve(argv.port)
+//     })
+//     .option('verbose', {
+//         alias: 'v',
+//         type: 'boolean',
+//         description: 'Run with verbose logging'
+//     })
+//     .parse()
+
 // const contacts = require('./contacts');
 
 // const main = async() => {
@@ -8,46 +118,18 @@
 //     contacts.saveContact(nama, email, noHP);
 // };
 
-const { tulisPertanyaan, saveContact } = require('./contacts');
+// const { tulisPertanyaan, saveContact } = require('./contacts');
 
-const main = async() => {
-    const nama = await tulisPertanyaan('Masukan nama anda: ');
-    const email = await tulisPertanyaan('Masukan email anda: ');
-    const noHP = await tulisPertanyaan('Masukan no handphone anda: ');
+// const main = async() => {
+//     const nama = await tulisPertanyaan('Masukan nama anda: ');
+//     const email = await tulisPertanyaan('Masukan email anda: ');
+//     const noHP = await tulisPertanyaan('Masukan no handphone anda: ');
 
-    saveContact(nama, email, noHP);
-}
-
-
-main();
+//     saveContact(nama, email, noHP);
+// }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// main();
 
 // const pertanyaan1 = () => {
 //     return new Promise((resolve, reject) => {
--- kemampuan-dasar/kemampuan-dasar-2/minggu-5/hari-1/contact-app/contacts.js
+++ kemampuan-dasar/kemampuan-dasar-2/minggu-5/hari-1/contact-app/contacts.js
@@ -1,10 +1,6 @@
 const fs = require('fs');
-
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
+const chalk = require('chalk');
+const validator = require('validator');
 
 const dirPath = './data';
 if (!fs.existsSync(dirPath)) {
@@ -17,24 +13,85 @@ if (!fs.existsSync(dataPath)) {
     fs.writeFileSync(dataPath, '[]', 'utf-8');
 }
 
-const tulisPertanyaan = (pertanyaan) => {
-    return new Promise((resolve, reject) => {
-        rl.question(pertanyaan, (nama, email, noHP) => {
-            resolve(nama, email, noHP);
-        });
-    });
-};
-
 const saveContact = (nama, email, noHP) => {
     const contact = { nama, email, noHP };
     const fileBuffer = fs.readFileSync('data/contacts.json', 'utf8');
     const contacts = JSON.parse(fileBuffer);
 
+    // cek validator email
+    if (email) {
+        if (!validator.isEmail(email)) {
+            console.log(
+                chalk.red.inverse.bold('Email tidak valid!!')
+            );
+            return false;
+        }
+    }
+
+    // cek validator nomor Handphone
+    if (email) {
+        if (!validator.isMobilePhone(noHP, 'id-ID')) {
+            console.log(
+                chalk.red.inverse.bold('No handphone tidak valid!!')
+            );
+            return false;
+        }
+    }
+
+    // cek duplikat nomor handphone
+    const duplikat = contacts.find((contact) => contact.noHP === noHP);
+    if (duplikat) {
+        console.log(
+            chalk.blue.inverse.bold('No handphone contact sudah terdaftar, ganti nama yang lain!!')
+        );
+        return false;
+    }
+
     contacts.push(contact);
 
     fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
-    console.log('Matur nuwun sanget melu program niki')
-    rl.close();
+    console.log(
+        chalk.blue.inverse.bold('Matur nuwun sanget melu program niki')
+    );
 };
 
-module.exports = { tulisPertanyaan, saveContact };
+module.exports = { saveContact };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // cek duplikat nama
+// const duplikat = contacts.find((contact) => contact.nama === nama);
+// if (duplikat) {
+//     console.log(
+//         chalk.green.inverse.bold('Nama Contact sudah terdaftar, ganti nama yang lain!!')
+//     );
+//     return false;
+// }
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+// });
+// const tulisPertanyaan = (pertanyaan) => {
+//     return new Promise((resolve, reject) => {
+//         rl.question(pertanyaan, (nama, email, noHP) => {
+//             resolve(nama, email, noHP);
+//         });
+//     });
+// };
