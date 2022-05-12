ab24347d39cf324d72b69535aa44cacda1120b82
oksipjogja <familybuah@gmail.com>
2022-05-12 16:37:18 +0700

    part-13

--- kemampuan-dasar/kemampuan-dasar-2/minggu-5/hari-1/express-contact/app.js
+++ kemampuan-dasar/kemampuan-dasar-2/minggu-5/hari-1/express-contact/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
-const { loadContact, findContact, addContact, cekDuplikat } = require('./utils/contacts');
+const {
+    loadContact,
+    findContact,
+    addContact,
+    cekDuplikat,
+    deleteContact,
+    updateContacts
+} = require('./utils/contacts');
 const { body, validationResult, check } = require('express-validator');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
@@ -15,6 +22,7 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
 // konfigurasi flash
+
 app.use(cookieParser('secret'));
 app.use(
     session({
@@ -22,8 +30,7 @@ app.use(
         secret: 'secret',
         resave: true,
         saveUnintialized: true,
-    })
-);
+    }));
 app.use(flash());
 
 app.get('/', (req, res) => {
@@ -100,12 +107,64 @@ app.post('/contact', [
     } else {
         addContact(req.body);
         // kirimkan flash message
-        req.flash('msg', 'Data contact add success');
+        req.flash('msg', 'Data contact sukses ditambahkan');
+        res.redirect('/contact');
+    }
+});
+// proses delete contact
+app.get('/contact/delete/:nama', (req, res) => {
+        const contact = findContact(req.params.nama);
+        // jika contact tidak ada
+        if (!contact) {
+            res.status(404);
+            res.send('<h1>404</h1>');
+        } else {
+            // res.send('Oye');
+            deleteContact(req.params.nama);
+            req.flash('msg', 'Data contact berhasil dihapus');
+            res.redirect('/contact');
+        }
+    })
+    // form ubah data contact
+app.get('/contact/edit/:nama', (req, res) => {
+    const contact = findContact(req.params.nama);
+    res.render('edit-contact', {
+        title: 'Form Ubah Data Contact',
+        layout: 'layouts/main-layout',
+        contact,
+    });
+});
+
+// proses ubah data
+app.post('/contact/update', [
+    body('nama').custom((value, { req }) => {
+        const duplikat = cekDuplikat(value);
+        if (value !== req.body.oldNama && duplikat) {
+            throw new Error('Asmanipun sampun diagem!');
+        }
+        return true;
+    }),
+    check('email', 'emailipun kelinthu').isEmail(),
+    check('nohp', 'no hp kelinthu').isMobilePhone('id-ID'),
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        // return res.status(400).json({ errors: errors.array() });
+        res.render('edit-contact', {
+            title: 'Form Ubah Data Contact',
+            layout: 'layouts/main-layout',
+            errors: errors.array(),
+            contact: req.body,
+        });
+    } else {
+        updateContacts(req.body);
+        // kirimkan flash message
+        req.flash('msg', 'Data contact berhasil diubah!');
         res.redirect('/contact');
     }
 });
 
-//detail
+// halaman detail contact
 app.get('/contact/:nama', (req, res) => {
     const contact = findContact(req.params.nama);
 
