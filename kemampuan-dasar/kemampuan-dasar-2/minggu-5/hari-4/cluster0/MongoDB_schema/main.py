9208858d645617e376cd3f7149a2eab0682bc79c
oksipjogja <familybuah@gmail.com>
2022-05-22 03:43:36 +0700

    validation mongodb

---
+++ kemampuan-dasar/kemampuan-dasar-2/minggu-5/hari-4/cluster0/MongoDB_schema/main.py
@@ -0,0 +1,105 @@
+from dotenv import load_dotenv, find_dotenv 
+import os
+import pprint
+from pymongo import MongoClient
+load_dotenv(find_dotenv())
+
+password = os.environ.get("MONGODB_PWD")
+
+
+connection_string = f"mongodb+srv://oksipjogja:{password}@cluster0.2bla9.mongodb.net/?retryWrites=true&w=majority&authSource=admin"
+
+client = MongoClient(connection_string)
+
+dbs = client.list_database_names()
+# sekolah = client.sekolah
+
+# validator = {
+#     "$jsonSchema": {
+#       "bsonType": "object",
+#       "required": [ "name", "year", "major", "address" ],
+#       "properties": {
+#           "name": {
+#             "bsonType": "string",
+#             "description": "must be a string and is required"
+#           },
+#           "year": {
+#             "bsonType": "int",
+#             "minimum": 2017,
+#             "maximum": 3017,
+#             "description": "must be an integer in [ 2017, 3017 ] and is required"
+#           },
+#           "major": {
+#             "enum": [ "Math", "English", "Computer Science", "History", "null" ],
+#             "description": "can only be one of the enum values and is required"
+#           },
+#           "gpa": {
+#             "bsonType": [ "double" ],
+#             "description": "must be a double if the field exists"
+#           },
+#           "address": {
+#             "bsonType": "object",
+#             "required": [ "city" ],
+#             "properties": {
+#                 "street": {
+#                   "bsonType": "string",
+#                   "description": "must be a string if the field exists"
+#                 },
+#                 "city": {
+#                   "bsonType": "string",
+#                   "description": "must be a string and is required"
+#                 }
+#             }
+#           }
+#       }
+#     }
+#   }
+
+# try:
+#   sekolah.create_collection("murid")
+# except Exception as e:
+#   print(e)   
+
+# sekolah.command("collMod", "murid", validator=validator)
+
+# publisher = client.publisher
+
+# validator = {
+#     "$jsonSchema": {
+#         "bsonType": "object",
+#         "required": [ "title", "authors", "publish_date", "type", "copies" ],
+#         "properties": {
+#           "title": {
+#               "bsonType": "string",
+#               "description": "must be a string and is required"
+#           },
+#           "authors": {
+#               "bsonType": "array",
+#               "items": {
+#                 "bsonType": "objectId",
+#                 "description": "must be a objectid and is required"
+#               }              
+#           },
+#           "publish_date": {
+#               "bsonType": "date",
+#               "description": "must be a date an is required"
+#           },
+#           "type": {
+#               "enum": [ "Fiction", "Non-Fiction" ],
+#               "description": "can only be one of the enum values and is required"
+#           },
+#           "copies": {
+#               "bsonType": "int",
+#               "minimum": "0",
+#               "description": "must be an integer greater than 0 and is required"
+#           },
+#         }
+#     }
+# }
+                                    
+# try:
+#   publisher.create_collection("book")
+# except Exception as e:
+#   print(e)   
+
+# publisher.command("collMod", "book", validator=validator)
