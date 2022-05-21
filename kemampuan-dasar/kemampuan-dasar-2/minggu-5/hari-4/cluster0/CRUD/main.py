04dd1b4e6c07bc9091ce9a2e3b1624b741ad14d6
oksipjogja <familybuah@gmail.com>
2022-05-21 22:30:16 +0700

    part 2 of crud

--- kemampuan-dasar/kemampuan-dasar-2/minggu-5/hari-4/cluster0/CRUD/main.py
+++ kemampuan-dasar/kemampuan-dasar-2/minggu-5/hari-4/cluster0/CRUD/main.py
@@ -33,9 +33,9 @@ client = MongoClient(connection_string)
 
 pabrik = client.pabrik
 karyawan_col = pabrik.karyawan
-# def buat_document():
 
-# # fungsi insertMany
+# melakukan input data denganfungsi insertMany
+# def buat_document():
 #   first_names = ["Dela", "Suwarno", "Wagiman", "Subagyo", "Parjiman", "Hendi"]
 #   last_names = ["Suwarni", "Hadiprawiro", "Harjolukito", "Makarti", "Suryodiningrat", "Oktavianus"]
 #   ages = [35, 40, 32, 33, 30, 25]
@@ -45,13 +45,11 @@ karyawan_col = pabrik.karyawan
 #   for first_name, last_name, age in zip(first_names, last_names, ages):
 #     doc = {"first_name" : first_name, "last_name" : last_name, "age" : age}
 #     docs.append(doc)
-    
 #   karyawan_col.insert_many(docs)
   
 # buat_document()
 
-
-### Fungsi find_all
+# Fungsi find_all
 printer = pprint.PrettyPrinter()
 
 # METHOD DEF FIND_ALL_n()
@@ -64,13 +62,46 @@ printer = pprint.PrettyPrinter()
 #     printer.pprint(karyawan)
     
 # find_all_orang()
+# ---> {'_id': ObjectId('6286a871cedb34fcf2215aab'),
+      #  'age': 32,
+      #  'first_name': 'Wagiman',
+      #  'last_name': 'Harjolukito'}
+      # donie@donie-ThinkPad-T430:~/praxis-academy/kemampuan-dasar/kemampuan-dasar-2/minggu-5/hari-4/cluster0/CRUD$ /bin/python3 /home/donie/praxis-academy/kemampuan-dasar/kemampuan-dasar-2/minggu-5/hari-4/cluster0/CRUD/main.py
+      # {'_id': ObjectId('6286a871cedb34fcf2215aa9'),
+      #  'age': 35,
+      #  'first_name': 'Dela',
+      #  'last_name': 'Suwarni'}
+      # {'_id': ObjectId('6286a871cedb34fcf2215aaa'),
+      #  'age': 40,
+      #  'first_name': 'Suwarno',
+      #  'last_name': 'Hadiprawiro'}
+      # {'_id': ObjectId('6286a871cedb34fcf2215aab'),
+      #  'age': 32,
+      #  'first_name': 'Wagiman',
+      #  'last_name': 'Harjolukito'}
+      # {'_id': ObjectId('6286a871cedb34fcf2215aac'),
+      #  'age': 33,
+      #  'first_name': 'Subagyo',
+      #  'last_name': 'Makarti'}
+      # {'_id': ObjectId('6286a871cedb34fcf2215aad'),
+      #  'age': 30,
+      #  'first_name': 'Parjiman',
+      #  'last_name': 'Suryodiningrat'}
+      # {'_id': ObjectId('6286a871cedb34fcf2215aae'),
+      #  'age': 25,
+      #  'first_name': 'Hendi',
+      #  'last_name': 'Oktavianus'}
   
   # CARA 2 : FIND__id
 # def find__id():
-#   _id = karyawan_col.find_one({ "_id" : ObjectId("6285d3eb8e7d281c4f7528ea") })
+#   _id = karyawan_col.find_one({ "_id" : ObjectId("6286a871cedb34fcf2215aab") })
 #   printer.pprint(_id)
   
 # find__id()
+# ---> {'_id': ObjectId('6286a871cedb34fcf2215aab'),
+#      'age': 32,
+#      'first_name': 'Wagiman',
+#      'last_name': 'Harjolukito'}
 
 #   # CARA 3 : FIND_NAME
 # def find_dela():
@@ -78,11 +109,70 @@ printer = pprint.PrettyPrinter()
 #   printer.pprint(dela)
   
 # find_dela()
+# # ---> {'_id': ObjectId('6286a871cedb34fcf2215aa9'),
+#        'age': 35,
+#        'first_name': 'Dela',
+#        'last_name': 'Suwarni'}
 
 # def count_all_orang():
 #   count = karyawan_col.count_documents(filter={})
 #   print("Jumlah karyawan : ", count)
   
 # count_all_orang()
+# --> Jumlah karyawan :  6                                                                                             SEJATI RENTAL                                                                                   PURWOKERTO
 
 
+# def cari_orang_by_id(karyawan_id):
+#   from bson.objectid import ObjectId
+  
+#   _id = ObjectId(karyawan_id)
+#   karyawan = karyawan_col.find_one({"_id": _id})
+#   printer.pprint(karyawan)
+  
+# cari_orang_by_id("6285d3eb8e7d281c4f7528ea")
+    
+# def cari_usia_range(min_age, max_age):
+#   query = {"$and" :  [
+#       {"age" : {"$gte" : min_age}},
+#       {"age" : {"$lte" : max_age}}
+#     ]}
+  
+#   orang = karyawan_col.find(query).sort("age")
+#   for karyawan in orang:
+#     printer.pprint(karyawan)
+    
+# cari_usia_range(30, 42)
+
+# def project_kolom():
+#   kolom = {"_id" : 0, "first_name" : 1, "last_name" : 1}
+#   orang = karyawan_col.find({}, kolom)
+#   for karyawan in orang:
+#     printer.pprint(karyawan)
+  
+# project_kolom()
+# ---> {'first_name': 'Dela', 'last_name': 'Suwarni'}
+      # {'first_name': 'Suwarno', 'last_name': 'Hadiprawiro'}
+      # {'first_name': 'Wagiman', 'last_name': 'Harjolukito'}
+      # {'first_name': 'Subagyo', 'last_name': 'Makarti'}
+      # {'first_name': 'Parjiman', 'last_name': 'Suryodiningrat'}
+      # {'first_name': 'Hendi', 'last_name': 'Oktavianus'}
+
+#  UPDATAE DOCUMENT
+# def update_person_by_id(karyawan_id):
+#   from bson.objectid import ObjectId
+  
+#   _id = ObjectId(karyawan_id)
+  
+#   all_update = {
+#     "$set": {"new_field": True},
+#     "$inc": {"age": 1},      # ---> $inc = increment/kenaikan
+#     "$rename": {"first_name": "first", "last_name": "last"}
+#   }
+#   karyawan_col.update_one({"_id": _id}, all_update)
+  
+#   update_person_by_id("6286a871cedb34fcf2215aad")
+# ===>
+# _id: ObjectId('6286a871cedb34fcf2215aad')  --> _id: ObjectId('6286a871cedb34fcf2215aad')    
+# first_name: "Parjiman"                     --> first: "Parjiman"
+# last_name: "Suryodiningrat"                --> last: "Suryodiningrat"
+# age: 30                                    --> age: 31
