b26480ec66a1063887735cbc1d9e787630dd3183
oksipjogja <familybuah@gmail.com>
2022-05-23 01:39:22 +0700

    pymongo arrow

--- kemampuan-dasar/kemampuan-dasar-2/minggu-5/hari-4/cluster0/MongoDB_advance_query/main.py
+++ kemampuan-dasar/kemampuan-dasar-2/minggu-5/hari-4/cluster0/MongoDB_advance_query/main.py
@@ -275,68 +275,68 @@ def create_data():
     {
       "first_name": "Andrew",
       "last_name": "Simson",
-      "date_of_birth": dt(1998, 4, 24)
+      "date_of_birth": dt(2000, 4, 24)
     },
     {
       "first_name": "Marshall",
       "last_name": "Edward",
-      "date_of_birth": dt(1994, 3, 14)
+      "date_of_birth": dt(1903, 3, 14)
     },
     {
       "first_name": "Billy",
       "last_name": "Jackson",
-      "date_of_birth": dt(1999, 8, 5)
+      "date_of_birth": dt(1819, 8, 5)
     },
     {
       "first_name": "Natalie",
       "last_name": "Cardwell",
-      "date_of_birth": dt(1997, 10, 2)
+      "date_of_birth": dt(1896, 10, 2)
     }
   ]
   
   author_col = penerbit.author
   authors = author_col.insert_many(authors).inserted_ids
   
-  books = [
-    {
-      "title": "MongoDB Advance Tutorial", 
-      "authors": [authors[1]], 
-      "publish_date": dt.today(), 
-      "type": "Non-Fiction", 
-      "copies": 5 
-    },
-    {
-      "title": "Python For Dummies", 
-      "authors": [authors[0]], 
-      "publish_date": dt(2000, 1, 17), 
-      "type": "Non-Fiction", 
-      "copies": 5 
-    },
-      {
-      "title": "Nineteen Eighty_Four", 
-      "authors": [authors[2]], 
-      "publish_date": dt(1949, 6, 8), 
-      "type": "Fiction", 
-      "copies": 5 
-    },
-      {
-      "title": "The Great Gatsby", 
-      "authors": [authors[3]], 
-      "publish_date": dt(2014, 5, 23), 
-      "type": "Fiction", 
-      "copies": 5 
-    },
-        {
-      "title": "Moby Dick", 
-      "authors": [authors[2]], 
-      "publish_date": dt(1851, 9, 24), 
-      "type": "Fiction", 
-      "copies": 5 
-    },
-  ]
+  # books = [
+  #   {
+  #     "title": "MongoDB Advance Tutorial", 
+  #     "authors": [authors[1]], 
+  #     "publish_date": dt.today(), 
+  #     "type": "Non-Fiction", 
+  #     "copies": 5 
+  #   },
+  #   {
+  #     "title": "Python For Dummies", 
+  #     "authors": [authors[0]], 
+  #     "publish_date": dt(2000, 1, 17), 
+  #     "type": "Non-Fiction", 
+  #     "copies": 5 
+  #   },
+  #     {
+  #     "title": "Nineteen Eighty_Four", 
+  #     "authors": [authors[2]], 
+  #     "publish_date": dt(1949, 6, 8), 
+  #     "type": "Fiction", 
+  #     "copies": 5 
+  #   },
+  #     {
+  #     "title": "The Great Gatsby", 
+  #     "authors": [authors[3]], 
+  #     "publish_date": dt(2014, 5, 23), 
+  #     "type": "Fiction", 
+  #     "copies": 5 
+  #   },
+  #       {
+  #     "title": "Moby Dick", 
+  #     "authors": [authors[2]], 
+  #     "publish_date": dt(1851, 9, 24), 
+  #     "type": "Fiction", 
+  #     "copies": 5 
+  #   },
+  # ]
   
-  book_col = penerbit.book
-  book_col.insert_many(books)
+  # book_col = penerbit.book
+  # book_col.insert_many(books)
   
 # create_data()
 
@@ -436,17 +436,99 @@ def create_data():
 #              'authors': [ObjectId('6289ed8628db7b4d41d9a4b7')],
 #              'copies': 5,
 #              'publish_date': datetime.datetime(2014, 5, 23, 0, 0),
-#              'title': 'The Great Gatsby',
+#              'title': 'The Great Gatsby'
 #              'type': 'Fiction'}],
 #   'date_of_birth': datetime.datetime(1997, 10, 2, 0, 0),
 #   'first_name': 'Natalie',
 #   'last_name': 'Cardwell'}]
 # --------------------------------------------------------------------
-authors_and_books = penerbit.author.aggregate([{
-  "$lookup": {
-    "from" : "book",
-    "localField": "_id",
-    "foreignField": "authors",
-    "as": "books"
-  }
-}])
+# authors_book_count = penerbit.author.aggregate([
+#   {
+#   "$lookup": {
+#     "from" : "book",
+#     "localField": "_id",
+#     "foreignField": "authors",
+#     "as": "books"
+#     }
+#   },
+#   {
+#     "$addFields": {
+#       "total_books": {"$size": "$books"}
+#     }
+#   },
+#   {
+#     "$project": {"first_name": 1, "last_name": 1, "total_books": 1, "_id": 0 }
+#   }
+# ])
+# printer.pprint(list(authors_book_count))
+# -----------------------------------------------------------------------------
+# [{'first_name': 'Andrew', 'last_name': 'Simson', 'total_books': 1},
+#  {'first_name': 'Marshall', 'last_name': 'Edward', 'total_books': 2},
+#  {'first_name': 'Billy', 'last_name': 'Jackson', 'total_books': 1},
+#  {'first_name': 'Natalie', 'last_name': 'Cardwell', 'total_books': 1}]
+# -----------------------------------------------------------------------------
+# books_with_old_authors = penerbit.book.aggregate([
+#   {
+#     "$lookup": {
+#       "from": "author",
+#       "localField": "authors",
+#       "foreignField": "_id",
+#       "as": "authors"
+#     }
+#   },
+#   {
+#     "$set": {
+#       "authors": {
+#         "$map": {
+#           "input": "$authors",
+#           "in": {
+#             "age": {
+#               "$dateDiff": {
+#                 "startDate": "$$this.date_of_birth",
+#                 "endDate": "$$NOW",
+#                 "unit": "year"
+#               }
+#             },
+#             "first_name": "$$this.first_name",
+#             "last_name": "$$this.last_name",
+#           }
+#         }
+#       }
+#     }
+#   },
+#   {
+#     "$match": {
+#       "$and": [
+#         {"authors.age": {"$gte": 51}},
+#         {"authors.age": {"$lte": 151}},
+#       ]
+#     }
+#   },
+#   {
+#     "$sort": {
+#       "age": 1
+#     }
+#   }
+# ])
+# printer.pprint(list(books_with_old_authors))
+# ---------------------- PYMONGO ARROW DEMO ----------------------------
+# python3 -m pip install jupyter pymongoarrow "pymongo[srv]" pandas numpy
+import pyarrow 
+from pymongoarrow.api import Schema
+from pymongoarrow.monkey import patch_all
+import pymongoarrow as pma 
+from bson import ObjectId
+
+patch_all() 
+
+author = Schema({"_id": ObjectId, "first_name": pyarrow.string(), "last_name":pyarrow.string(), "date_of_birth": dt})
+
+df = penerbit.author.find_pandas_all({}, schema=author)
+print(df.head())
+# -----------------------------------------------------------------------
+# _id first_name last_name date_of_birth
+# 0  b'b\x8ajP<\xbcc\xe0d\x1a\x9a\\'     Andrew    Simson    2000-04-24
+# 1   b'b\x8ajP<\xbcc\xe0d\x1a\x9a]'   Marshall    Edward    1903-03-14
+# 2   b'b\x8ajP<\xbcc\xe0d\x1a\x9a^'      Billy   Jackson    1819-08-05
+# 3   b'b\x8ajP<\xbcc\xe0d\x1a\x9a_'    Natalie  Cardwell    1896-10-02
+# -----------------------------------------------------------------------
